8a055da126f4317fbdbe19ec24d089194bf250fc
Ivana Konatar <ivana.konatar@udg.edu.me>
2018-09-14 16:33:35 +0200

    Homework00

---
+++ task1.py
@@ -0,0 +1,24 @@
+def minmax(*arguments):
+
+    for i in arguments:
+
+        if not isinstance(i, int) and not isinstance(i, float):
+            print("Please enter numbers!")
+            quit()
+
+    smallest_number = arguments[0]
+
+    largest_number = arguments[0]
+
+    for i in arguments[1: ]:
+
+        if i < smallest_number:
+            smallest_number = i
+
+        if i > largest_number:
+            largest_number = i
+
+    return smallest_number,largest_number
+
+
+print(minmax(3,5.7,6,-4))
